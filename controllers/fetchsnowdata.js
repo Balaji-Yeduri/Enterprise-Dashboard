91172c5038642b5d359882d90ce0144c8a1ef21f
Balaji-Yeduri <balaji.yeduri@ttec.com>
2022-06-15 20:42:35 +0530

    pushing route changes

--- controllers/fetchsnowdata.js
+++ controllers/fetchsnowdata.js
@@ -1,78 +1,143 @@
 const config = require('../config');
 const axios = require('axios');
 
+//function fetchsnowdata(){
+  //const fetchsnowdata = async () => {
+global.DBIncidentArray=[];
+
 
 let MongoClient = require('mongodb').MongoClient;
 var lastruntime;
 var asknowquerystring;
-MongoClient.connect('mongodb://'+config.mongo.host, function(err, client){
-  if(err) throw err;
-  let db = client.db('Enterprise__dashboard');
-  //read last runtime
+global.db;
+let client;
+
+
+const dbconnect = () => {
+  return new Promise((resolve, reject) => {
+ 
+  MongoClient.connect('mongodb://'+config.mongo.host, function(err, client) {
+       if (err) {
+            console.log('Mongo db connection Error', err); 
+            resolve('false');
+       } else {
+
+          db = client.db('Enterprise__dashboard');
+          console.log('db connected');
+          resolve(db);
+       }
+  });
+
+})
+};
+
+const fetchlastruntime = (db) => {
+  return new Promise((resolve, reject) => {
+ //let lastruntime = false;
+
   db.collection('Lastruntime').find().toArray(function(err, result){
     if(err)
     {
-        console.log('Error Reading Database');
+        console.log('Error Reading table');
+        resolve(false);
     }
     else{
-    console.log(result[0]['datetime']);
     lastruntime = result[0]['datetime'];
-    //update current datetime in db 
+    console.log(lastruntime);
     let date_ob = new Date();
     var monthcount=date_ob.getMonth()+1;
     var currentruntime=date_ob.getFullYear()+'-'+monthcount+'-'+date_ob.getDate()+' '+date_ob.getHours()+':'+date_ob.getMinutes()+':'+date_ob.getMinutes()
     console.log(currentruntime);
     //db.collection('Lastruntime')findOneAndUpdate({ datetime':"'2021-10-01','07:44:00' },{$set: {'datetime':"'2021-01-01','07:44:00'"}})
-    
+    resolve(lastruntime);
+  }
+       });
+  
+})
+}
 
 
-    //get asknow projects list from db
+const asknowprojectslist = (db) => {
+  return new Promise((resolve, reject) => {
+//get asknow projects list from db
    
     db.collection('Projects_list').find({"monitoringType":"Ask Now"}).toArray(function(err, result){
       if(err){
       console.log(err);
+      resolve(false);
     }
       else
       { 
-        var asknowquerystring='';
+        asknowquerystring='';
         for (var arrayindex = 0; arrayindex < result.length; arrayindex++) 
         { 
           asknowquerystring= asknowquerystring+'cmdb_ci='+result[arrayindex]['sysId']+'^OR';
          }
          asknowquerystring='^'+asknowquerystring;
          console.log(asknowquerystring);
+
+         resolve(asknowquerystring);
       }
+      
     })
-    
-    
-    
-    
-    //get api call
-    let inputdata = {
+});
+}
+
+
+const fetchdata =  (lastruntime,db,asknowquerystring) =>{
+  return new Promise((resolve, reject) => {
+  
+      //get api call
+      console.log(lastruntime);
+      let inputdata = {
         headers: {'Authorization': config.asknow.auth},
         params: {
-         'sysparm_fields': 'number,u_outage_flag,business_service,priority,incident_state,cmdb_ci.sys_id',
+         'sysparm_fields': 'number,u_outage_flag,priority,incident_state,cmdb_ci.name',
          //'sysparm_limit':1,
-         'sysparm_query':"priorityIN1,2,3^incident_stateIN2,3,4,5^sys_created_on>javascript:gs.dateGenerate('"+lastruntime+"')"+'^cmdb_ci=d8b3591f6f799200ac14373aea3ee42c'//asknowquerystring
+         'sysparm_query':"priorityIN1,2,3^incident_stateIN2,3,4,5^sys_created_on>javascript:gs.dateGenerate('"+lastruntime+"')"+asknowquerystring
         }
       }
      axios.get(config.asknow.host+'/api/now/table/incident', inputdata)
     .then((res) => {
       console.log(res.data);
-      let respdata =[];
-      respdata= res.data;
-      console.log(respdata.length);
-      // for(inccount=0;inccount<res.data.length;inccount++)
-      // {
-      //   console.log(res.data[inccount]);
-      // }
-
+      console.log(res.data.result.length);
+      if(res.data.result.length && res.data.result.length >0){
+        var inputobj=[];
+        for(inccounter=0;inccounter<res.data.result.length;inccounter++){
+          inputobj.push({'Inc_Number': res.data.result[inccounter].number,'outage': res.data.result[inccounter].u_outage_flag,'Inc_State': res.data.result[inccounter].incident_state,'Business_Service':res.data.result[inccounter]['cmdb_ci.name'],'Priority':res.data.result[inccounter].priority});
+          }
+          db.collection("Incident_list").insertMany(inputobj); 
+      }
+      db.collection('Incident_list').find().toArray(function(err, DBIncidentArray){
+        console.log(DBIncidentArray.length);
+        resolve(DBIncidentArray);
+      })
     })
     .catch((error) => {
       console.error(error)
     })
-    //client.close();
-    }
-    });
- });
+ 
 
+});
+}
+// const closedbconn = (db) => {
+//   return new Promise((resolve, reject) => {
+//     db.close();
+
+// });
+// }
+
+function fetchsnowdata(){   
+  return new Promise((resolve, reject) => {
+  dbconnect()
+  .then(db => fetchlastruntime(db))
+  .then(lastruntime => asknowprojectslist(db,lastruntime))
+  .then(asknowquerystring => fetchdata(lastruntime,db,asknowquerystring))
+  .then(DBIncidentArray =>  resolve(DBIncidentArray))
+
+  
+});
+}
+//fetchsnowdata();
+
+module.exports = { fetchsnowdata };
--- server.js
+++ server.js
@@ -2,30 +2,57 @@ const express               =  require('express'),
       app                   =  express(),
       mongoose              =  require("mongoose"),
       bodyParser            =  require("body-parser");
-     // User                  =  require("./models/ProjectModel.js");
-     const path = require('path');
+      User                  =  require("./models/ProjectModel.js");
+      cron = require('node-cron');
+      const path = require('path');
+const { fetchsnowdata } = require('./controllers/fetchsnowdata.js');
+global.DBIncidentArray;
 
-
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'views')));
-//Connecting database
-//mongoose.connect("mongodb://localhost/auth_demo");
-//=======================
-//      R O U T E S
-//=======================
-app.get("/", (req,res) =>{
-   res.sendFile(path.join(__dirname,'views','index.html'));
-});
 
-//Auth Routes
-// app.get("/login",(req,res)=>{
-//     res.render("login");
-// });
-//Listen On Server
-app.listen(process.env.PORT ||3000,function (err) {
-    if(err){
-        console.log(err);
-    }else {
-        console.log("Server Started At Port 3000");
-    }
-      
-});
+
+
+
+fetchsnowdata().then(DBIncidentArray => {
+    // //=======================
+    //      R O U T E S
+    //=======================
+    app.get(["/","/index.html"], (req,res) =>{
+
+        res.render('index.ejs', {incarray : DBIncidentArray})
+     });
+     app.get("/project-management.html", (req,res) =>{
+
+        res.render('project-management.ejs', {})
+     });
+    
+    app.get("/analytics.html", (req,res) =>{
+    
+        res.render('analytics.ejs', {})
+     });
+    
+    app.get("/tickets.html", (req,res) =>{
+    
+        res.render('tickets.ejs', {incarray : DBIncidentArray})
+     });
+
+
+     cron.schedule('* * * * *', function() {
+        console.log('running a task every minute');
+        fetchsnowdata().then(currentresponse =>{
+            DBIncidentArray = currentresponse;
+        });
+      });
+    
+  });
+
+  app.listen(3000, () => {
+    console.log('Listening on PORT: 3000')
+  });
+    
+
+
+
+
+
